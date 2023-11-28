b934205e98ce675ea151cc91fc22d98ff33ce0a8
Tiesen <ttien56906@gmail.com>
2023-11-28 16:36:35 +0700

    seo

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -1,17 +1,10 @@
-import { baseUrl } from '@/lib/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 
 const Logo: React.FC = () => (
   <Link href="/" legacyBehavior passHref>
     <article className="flex cursor-pointer items-center space-x-2">
-      <Image
-        src={`${baseUrl}/logo.png`}
-        alt="logo"
-        width={40}
-        height={40}
-        className="rounded-full"
-      />
+      <Image src="/logo.webp" alt="logo" width={40} height={40} className="rounded-full" />
       <section className="flex flex-col">
         <span className="font-medium">Tiesen</span>
         <small className="text-gray-400 transition-colors hover:text-gray-300">@tiesen243</small>
