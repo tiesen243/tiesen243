10e26d7707e63eeeb3bbe4fa4aa20c1f2904bb80
Tiesen <ttien56906@gmail.com>
2023-12-01 05:04:30 +0700

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://tiesen.id.vn/',
+      url: 'https://www.tiesen.id.vn/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://acme.com/blogs',
+      url: 'https://www.tiesen.id.vn/blogs',
       lastModified: new Date(),
       changeFrequency: 'always',
       priority: 0.8,
     },
     {
-      url: 'https://acme.com/projects',
+      url: 'https://www.tiesen.id.vn/projects',
       lastModified: new Date(),
       changeFrequency: 'always',
       priority: 0.5,
