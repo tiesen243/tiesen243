f5ccf78876c9d53405931c86458faa45a82fd563
tiesen243 <ttien56906@gmail.com>
2023-12-31 20:21:25 +0700

    aaa

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import '@/lib/syntax.modules.css'
 const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
   <div className="h-screen w-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll">
     <Header />
-    <main className="container mb-20 max-w-screen-md">{children}</main>
+    <main className="container mb-20 mt-4 max-w-screen-md">{children}</main>
   </div>
 )
 
