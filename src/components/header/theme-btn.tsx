350b249022050d2a6fb7f085592b617c6dce13dd
tiesen243 <ttien56906@gmail.com>
2024-01-15 18:45:37 +0700

    dasdaas

--- src/components/header/theme-btn.tsx
+++ src/components/header/theme-btn.tsx
@@ -1,17 +1,22 @@
 'use client'
 
 import { Button } from '@nextui-org/react'
-import { MoonIcon } from 'lucide-react'
+import { MoonIcon, SunIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 const ThemeBtn: React.FC = () => {
   const { theme, setTheme } = useTheme()
 
   const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
 
+  const [mounted, setMounted] = useState<boolean>(false)
+  useEffect(() => setMounted(true), [])
+  if (!mounted) return <Button aria-label="theme-btn" variant="bordered" isIconOnly isLoading />
+
   return (
     <Button aria-label="theme-btn" onClick={toggleTheme} variant="bordered" isIconOnly>
-      {theme === 'light' ? <MoonIcon /> : <MoonIcon />}
+      {theme === 'light' ? <MoonIcon /> : <SunIcon />}
     </Button>
   )
 }
