2dafac561bd4df9453f6e962020b6fa6d1a2cc8b
tiesen243 <ttien56906@gmail.com>
2024-02-05 12:52:44 +0700

    remove unuse dependences

--- components/customMdxComponents.tsx
+++ components/customMdxComponents.tsx
@@ -1,11 +1,12 @@
-import { MDXComponents } from 'mdx/types'
 import Image, { ImageProps } from 'next/image'
 import { Button, ButtonProps } from './ui/button'
+import type { MDXComponents } from 'mdx/types'
 
 const customMdxComponents: MDXComponents = {
   a: (props) => <a {...props} target="_blank" rel="noopener noreferrer" />,
-  // eslint-disable-next-line jsx-a11y/alt-text
-  img: (props) => <Image {...(props as ImageProps)} width={1920} height={1080} />,
+  img: (props) => (
+    <Image {...(props as ImageProps)} alt={props.alt ?? ''} width={1920} height={1080} />
+  ),
   button: (props) => <Button {...(props as ButtonProps)} />,
   code: (props) => <code {...props} className="font-mono" />,
   pre: (props) => <pre {...props} className="*:selection:bg-white *:selection:text-black" />,
