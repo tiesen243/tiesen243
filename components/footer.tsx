45284768bd6ff6a1e35e9a11a2f7a1671f796bea
tiesen243 <ttien56906@gmail.com>
2024-02-21 13:37:49 +0700

    edit footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,11 @@ import { ThemeBtn } from './theme-provider'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="mt-4 border-t p-4">
+    <footer className="mt-4 border-t py-4">
       <div className="container flex items-center justify-between">
         <p>
-          &copy; CopyRight {new Date().getFullYear()} | Developed by <Link href="/">Tiesen</Link>.
-          All rights reserved.
+          &copy; CopyRight {new Date().getFullYear()} | <Link href="/">Tiesen</Link>. All rights
+          reserved.
         </p>
 
         <ThemeBtn />
