9c891099dcb6a73cc50745ad1d7da5335d8a319e
tiesen243 <ttien56906@gmail.com>
2024-02-21 13:35:24 +0700

    reponsive

--- components/header.tsx
+++ components/header.tsx
@@ -12,7 +12,7 @@ const Header: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false)
 
   return (
-    <header className="fixed inset-0 z-50 flex h-fit justify-start p-4 pb-0 md:justify-end">
+    <header className="fixed inset-0 z-50 flex h-fit justify-start p-2 pb-0 md:justify-end">
       <ul className="hidden items-center gap-4 md:flex">
         {siteConfig.navLinks.map((link) => (
           <li key={link.url}>
@@ -25,25 +25,25 @@ const Header: React.FC = () => {
 
       <button aria-label="menu-toggle" className="block md:hidden" onClick={() => setOpen(!open)}>
         <XIcon
-          size={32}
+          size={24}
           className={`absolute transition-all duration-500 ease-linear ${open ? 'rotate-180' : 'rotate-0 opacity-0'}`}
         />
         <MenuIcon
-          size={32}
+          size={24}
           className={`transition-all duration-500 ease-linear ${open ? 'rotate-180 opacity-0' : 'rotate-0'}`}
         />
       </button>
 
       <MotionDiv
-        className="absolute flex h-fit w-56 gap-4 p-4 md:hidden"
+        className="absolute flex h-fit w-56 gap-4 p-2 md:hidden"
         initial={{ y: '-100%', opacity: 0 }}
         animate={{ y: open ? 0 : '-100%', opacity: open ? 1 : 0 }}
         transition={{ duration: 0.5 }}
         onClick={() => setOpen(false)}
       >
-        <div className="mt-8 h-auto w-[2px] bg-gradient-to-b from-primary/60 via-primary to-primary/60" />
+        <div className="mt-6 h-auto w-[2px] bg-gradient-to-b from-primary/60 via-primary to-primary/60" />
 
-        <Card className="mt-8 w-full bg-background p-4 shadow-lg">
+        <Card className="mt-6 w-full bg-background p-4 shadow-lg">
           <nav className="flex flex-col justify-center space-y-4">
             {siteConfig.navLinks.map((link) => (
               <Link
