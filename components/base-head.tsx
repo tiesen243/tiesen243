801fe3a75aae4ac8d521eef8199e6dd6c795afbc
tiesen243 <ttien56906@gmail.com>
2024-04-02 15:51:59 +0700

    dasd

--- components/base-head.tsx
+++ components/base-head.tsx
@@ -4,14 +4,14 @@ import Head from 'next/head'
 interface Props {
   title?: string
   description?: string
-  images?: string
+  image?: string
   slug: string
   children?: React.ReactNode
 }
 export const BaseHead: React.FC<Props> = (props) => {
   const title = props.title ? `${props.title} | Tiesen` : 'Tiesen'
   const description = props.description ?? 'A collection of blogs written by me'
-  const images = props.images ?? '/og.jpg'
+  const image = props.image ?? '/og.jpg'
 
   return (
     <>
@@ -20,14 +20,14 @@ export const BaseHead: React.FC<Props> = (props) => {
         <meta name="description" content={description} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={images} />
+        <meta property="og:image" content={image} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content={`${baseUrl}/blog/${props.slug}`} />
         <meta name="twitter:site" content="@tiesen243" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={images} />
+        <meta name="twitter:image" content={image} />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
