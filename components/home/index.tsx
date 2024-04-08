d7edbbcc51eba9c411b0181b37fa9651142e17bb
tiesen243 <ttien56906@gmail.com>
2024-04-08 17:42:49 +0700

    dasdasaa

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,10 +1,8 @@
 import { ButtonGroup } from './button-group'
 import { Intro } from './intro'
-import { Meteors } from './meteor'
 
 export const Home: React.FC = () => (
   <section id="home" className="grid min-h-dvh place-items-center">
-    <Meteors />
     <div className="max-w-screen-md">
       <Intro />
       <ButtonGroup />
