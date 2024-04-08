dde4302e0b3f20fec4c01925098a06e18b65d5ed
tiesen243 <ttien56906@gmail.com>
2024-04-08 17:59:23 +0700

    dsad

--- components/about/skills.tsx
+++ components/about/skills.tsx
@@ -11,7 +11,7 @@ export const Skills: React.FC = () =>
         <ul
           key={j}
           className={cn(
-            'flex items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-1',
+            'flex items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-2',
             i === 0 ? 'animate-infinite-scroll' : 'animate-infinite-scroll-reverse',
           )}
           aria-hidden={j > 0}
